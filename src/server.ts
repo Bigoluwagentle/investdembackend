3b713a3ac885b36d78da7af749b3e68e5a93cd45
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-01-24 05:34:58 -0800

    updating server.js

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
