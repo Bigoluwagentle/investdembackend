289cd1e0b7728c7ee83a6e2f7e211e3fb813cf8d
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-01-27 05:58:19 -0800

    updating routes

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/adminController";
+import { createUser, assignCoinToUser } from "../controllers/adminController";
 import { protect, adminOnly } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/create-user", protect, adminOnly, createUser);
+router.post("/assign-coin", protect, adminOnly, assignCoinToUser);
 
 export default router;
