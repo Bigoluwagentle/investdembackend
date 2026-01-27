659777b7b9907629fed528fa3d7a4db53b76bba2
bigoluwagentle <olayiwolaolabode06@gmail.com>
2026-01-27 05:25:54 -0800

    updating

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,18 +1,34 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 import User from "../models/User";
 
-export const createUser = async (req: any, res: any) => {
-  const { email, password } = req.body;
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password, balance, coins } = req.body;
 
-  const user = await User.create({
-    email,
-    password,
-    role: "user",
-  });
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-  res.json(user);
-};
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-export const getAllUsers = async (_: any, res: any) => {
-  const users = await User.find({ role: "user" });
-  res.json(users);
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      role: "user",
+      balance,
+      coins,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
